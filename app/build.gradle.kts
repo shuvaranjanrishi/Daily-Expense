ee06c854c12097829f87c3867c79cb0bcd8f1630
shuvaranjanrishi <shopnershopnil44@gmail.com>
2026-02-23 15:22:11 +0600

    version 1.0.3 released

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.therishideveloper.dailyexpense"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.2"
+        versionCode = 3
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
